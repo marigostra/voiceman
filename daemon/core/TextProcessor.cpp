1f33da53fa7aadd2130bf60c7d9ee80943dfc55b
Michael Pozhidaev <msp@altlinux.ru>
2013-02-06 03:28:53 +0700

    Experimental rate reducing for letters

--- daemon/core/TextProcessor.cpp
+++ daemon/core/TextProcessor.cpp
@@ -70,6 +70,8 @@ void TextProcessor::split(const std::wstring& text, TextItemList& items) const
 
 void TextProcessor::processLetter(wchar_t c, TextParam volume, TextParam pitch, TextParam rate, TextItemList& items) const
 {
+  TextParam faster = rate;
+  faster -= 10;
   LangId langId = LANG_ID_NONE;
   WCharToLangIdMap::const_iterator it = m_charsTable.find(c);
   if (it != m_charsTable.end())
@@ -82,7 +84,7 @@ void TextProcessor::processLetter(wchar_t c, TextParam volume, TextParam pitch, 
     {
       WCharToWStringMap::const_iterator specialValueIt=m_specialValues.find(c);
       if (specialValueIt != m_specialValues.end())
-	process(TextItem(specialValueIt->second, volume, pitch, rate), items);
+	process(TextItem(specialValueIt->second, volume, pitch, faster), items);
       return;
     }
   const Lang* lang = getLangById(langId);
@@ -92,12 +94,12 @@ void TextProcessor::processLetter(wchar_t c, TextParam volume, TextParam pitch, 
   WCharToWStringMap::const_iterator specialValueIt=m_specialValues.find(c);
   if (specialValueIt != m_specialValues.end())
     {
-      process(TextItem(specialValueIt->second, volume, p, rate), items);
+      process(TextItem(specialValueIt->second, volume, p, faster), items);
       return;
     }
   std::wstring s;
   s += c;
-  TextItem item(langId, s, volume, p, rate);
+  TextItem item(langId, s, volume, p, faster);
   item.mark(0);
   items.clear();
   items.push_back(item);
